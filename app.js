341fcfd36881a4837e829bd8436e99f3ffcd2bf9
lucek93 <luc.jaroslaw@gmail.com>
2020-06-14 16:17:50 +0200

    Port fix

--- app.js
+++ app.js
@@ -180,9 +180,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
     port = 3000;
 }
-app.listen(port);
-
-
 
 app.listen(port, () => {
     console.log("Server has started successfully");
